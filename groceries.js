a08247705e1a6787fd17cd1b86c6a14f919e2244
Jorge Ceja-Carrillo <jceja-carrilio@csuchico.edu>
2016-12-01 22:42:31 -0800

    finish

--- groceries.js
+++ groceries.js
@@ -1,27 +1,10 @@
+window.onload = loadCookieList
 var myList = [] ;
 function addItem(){
   var input = document.getElementById("newItem").value;
-  myList.push(input);
-  console.log(myList);
-  myList.findIndex(input)
-      return -1
-    if(myList = -1){
 
-    }
-
-  var list = document.getElementById("listDisplay");
-  var item = document.createElement("li");
-  var btnClose = document.createElement("button")
-  btnClose.addEventListener("click", removeParentListItem);
-  btnClose.classList.add("btn","btn-danger","btn-xs");
-  iconClose = document.createElement("span");
-  iconClose.classList.add("glyphicon","glyphicon-remove");
-  btnClose.appendChild(iconClose);
-  item.appendChild(btnClose);
-  var itemName = document.createTextNode(input);
-  item.appendChild(itemName);
-  list.appendChild(item);
-  document.getElementById("newItem").value = "";
+ document.getElementById("newItem").value = "";
+  displayItem(foodList);
 }
 function removeParentListItem(){
   var mom = this.parentNode;
@@ -29,6 +12,7 @@ function removeParentListItem(){
   grandma.removeChild(mom);
   itemRemove = mom.firstChild.textContent;
   itemIndex = myList(itemRemove);
+  myList.splice(itemIndex,1);
 }
 function saveList(){
   var newList = myList.toString();
@@ -40,7 +24,41 @@ function clearList(){
    allNothing.innerHTML= "";
   myList = [];
 }
+function displayItem(foodList){
+  var foodList;
+  var index;
+  index = myList.findIndex(foodList);
+    if(index == -1){
+    var item =  document.createElement("li");
+    myList.push(input);
+    console.log(myList);
+    myList = myList.findIndex;
 
+    var list = document.getElementById("listDisplay");
+    var item = document.createElement("li");
+    var btnClose = document.createElement("button")
+    btnClose.addEventListener("click", removeParentListItem);
+    btnClose.classList.add("btn","btn-danger","btn-xs");
+    iconClose = document.createElement("span");
+    iconClose.classList.add("glyphicon","glyphicon-remove");
+    btnClose.appendChild(iconClose);
+    item.appendChild(btnClose);
+    var itemName = document.createTextNode(input);
+    item.appendChild(itemName);
+    list.appendChild(item);
+    }
+
+
+}
+function loadCookieList(){
+  var Glist = getCookie("listA");
+  var arrayCookie = Glist.split(" ");
+  var i;
+  for (i =0; i< arrayCookie.length; i++){
+ displayItem();
+  document.createElement("li");
+  }
+}
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
